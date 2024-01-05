4a22725a04ffcaadbe39945ae2b4deebd31f8f6d
Dmitry Melnikov <melnikovdv@gmail.com>
2024-01-05 15:14:31 +0000

    TRE-905: Update UserFlow sample app

--- Android/userflow-android-example/app/src/main/java/com/example/sampleapp/MainFragment.kt
+++ Android/userflow-android-example/app/src/main/java/com/example/sampleapp/MainFragment.kt
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.productscience.userflow.UserFlow
+import com.productscience.userflow.v2.UserFlow
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainFragment: Fragment() {
-    var hasResumed = false
 
     private lateinit var binding: FragmentMainBinding
 
@@ -25,29 +24,24 @@ class MainFragment: Fragment() {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Default) {
             // We will use Thread#sleep to simulate loading an image from the network
-            UserFlow.custom(APP_START, "Loading avatar")
+            UserFlow.custom(APP_START_USER_FLOW_ID, "Loading avatar")
             Thread.sleep(500)
-            UserFlow.custom(APP_START, "Avatar loaded")
             withContext(Dispatchers.Main) {
                 binding.progressAvatar.visibility = View.GONE
                 binding.avatar.visibility = View.VISIBLE
+                UserFlow.custom(APP_START_USER_FLOW_ID, "Avatar loaded")
             }
-            UserFlow.custom(APP_START, "Loading banner")
             Thread.sleep(500)
-            UserFlow.custom(APP_START, "Banner loaded")
+            UserFlow.custom(APP_START_USER_FLOW_ID, "Loading banner")
+            Thread.sleep(500)
             withContext(Dispatchers.Main) {
                 binding.progressBanner.visibility = View.GONE
                 binding.bannerimage.visibility = View.VISIBLE
+                UserFlow.custom(APP_START_USER_FLOW_ID, "Banner loaded")
             }
+            Thread.sleep(500)
+            UserFlow.end(APP_START_USER_FLOW_ID)
         }
         return binding.root
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (!hasResumed) {
-            UserFlow.end(APP_START, "App start complete")
-            hasResumed = true
-        }
     }
 }
--- Android/userflow-android-example/app/src/main/java/com/example/sampleapp/UserFlowIds.kt
+++ Android/userflow-android-example/app/src/main/java/com/example/sampleapp/UserFlowIds.kt
@@ -1,3 +1,3 @@
 package com.example.sampleapp
 
-const val APP_START = 1
+const val APP_START_USER_FLOW_ID = "appStart"
